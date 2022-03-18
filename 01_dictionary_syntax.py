afefae370994f2b5bb1d11466ff9a9acbcce06fd
Rajendra <rajendra_kulkarni1@rediffmail.com>
2022-03-18 16:48:58 +0530

    Transferring to github

---
+++ 01_dictionary_syntax.py
@@ -0,0 +1,13 @@
+# Dictionary and sets 
+# dictionary is a collection of key-value pairs 
+# Syntax: keys   values
+# a = {"keys" : "values"
+# "Rajendra" : "coder"
+# "marks" : "100"
+# "list" : [7, 3, 4, 9]}
+myDict = {
+    "Rajendra" : "Coder", "marks" : "100", "list" : [7, 3, 4, 9]
+}
+print(myDict["Rajendra"])
+print(myDict["marks"])
+print(myDict["list"])
---
+++ dictionary_methods.py
@@ -0,0 +1,13 @@
+# Consider the following dictionary
+a = { "name" : "Rajendra",
+"from" : "India",
+"marks" : [97, 98, 99]
+}
+print(a.keys())
+print(a.values())
+print(a.items())
+print(a)
+updatea = {"wife" : "bhavana",
+}
+a.update(updatea)
+print(a)
